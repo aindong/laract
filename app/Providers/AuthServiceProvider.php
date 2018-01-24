83a45c87646b03c4b9e59e52f2fb848a3675c722
Alleo Indong <freedom_war550@live.com.ph>
2018-01-24 13:22:57 +0800

    updated auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes();
+
+        // Expiration of token and refresh token
+        Passport::tokensExpireIn(now()->addDays(5));
+        Passport::refreshTokensExpireIn(now()->addDays(10));
     }
 }
