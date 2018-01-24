3c3255613c5f65083ce30af72296c808afeadc03
Alleo Indong <freedom_war550@live.com.ph>
2018-01-24 16:38:58 +0800

    updated main.js file

--- resources/assets/js/Main.js
+++ resources/assets/js/Main.js
@@ -29,7 +29,7 @@ class Main extends Component {
         return (
             <div style={containerStyle}>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <Link className="navbar-brand" to="/">Navbar</Link>
+                    <Link className="navbar-brand" to="/">LARACT</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
